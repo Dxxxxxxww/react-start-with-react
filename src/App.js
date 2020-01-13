d38a4d199208638fbe58d0dc70d3c7c20d3f5da3
Dxxxxxxww <gotu2018@163.com>
2020-01-13 22:00:12 +0800

    跟着官网学习 react 1-3章 by dxw

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const author = {
+  firstName: 'xx',
+  lastName: 'D'
+}
+
+const formatName = function (author) {
+  return `${author.firstName} ${author.lastName}`
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>Author: {formatName(author)}</p>
+			</header>
+		</div>
+	)
 }
 
 export default App;
